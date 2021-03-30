d9da2e710e4a9ff274b2910316442acca1897a01
Omar-aly <omar.aly@sci.ku.edu.kw>
2021-03-30 23:37:51 +0300

    Added First three files

---
+++ temp.js
@@ -0,0 +1,28 @@
+const greeting = () => console.log('Hello, world!');
+
+greeting();
+
+
+
+const sum = function() {
+    var val1 = 2; var val2 = 5; console.log(val1+val2);
+}
+
+sum();
+
+var string = 'This is a temp workspace for javascript       ';
+var temp = string.trim();
+console.log(temp);
+var temp2 = string.toUpperCase();
+console.log(temp2);
+
+var sentence = 'This is a temp sentence';
+sentence === 'This is a temp sentence' ? console.log('True') : console.log('False');
+
+
+const func = word => {
+	console.log(word);
+}
+
+func('hello');
+
