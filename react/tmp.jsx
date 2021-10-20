5b88842370966c44c67bb150a3d8f5c8a3ca1ac0
Omar-aly <omar.aly@sci.ku.edu.kw>
2021-10-20 10:03:00 +0300

    tmp file for JSX testing

---
+++ react/tmp.jsx
@@ -0,0 +1,23 @@
+const animals = {
+    dolphin: {
+      image: '/images/dolphin.jpg',
+      facts: ['Dolphins have been shown to give distinct names to each other!', 'Dolphins are known to display their own culture!', 'Dolphins have two stomachs!']
+    },
+    lobster: {
+      image: '/images/lobster.jpg',
+      facts: ['Lobsters taste with their legs!', 'Lobsters chew with their stomachs!', 'Lobsters can live as long as 100 years.']
+    },
+    starfish: {
+      image: '/images/starfish.jpg',
+      facts: ['Starfish can have up to 40 arms!', 'Starfish have no brain and no blood!', 'Starfish can regenerate their own arms!']
+    }
+  };
+  
+  
+  const images = []
+  for (const animal in animals) {
+      images.push(`<img key=${animal} className='animal' alt=${animal} src=${animals[animal].image} aria-label=${animal} role='button'/>`);
+  }
+
+
+  console.log(images.length);
